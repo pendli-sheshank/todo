b445869069e82f8870b8677fcc600d4008cb8c59
sheshank <sheshank3336@gmail.com>
2022-02-13 23:14:04 +0530

    todolist react with localstorage

---
+++ src/LIstTodos.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class LIstTodos extends Component {
+  render() {
+    return (
+      <div>
+        <center>List of ToDo</center>
+      </div>
+    );
+  }
+}
+
+export default LIstTodos;
